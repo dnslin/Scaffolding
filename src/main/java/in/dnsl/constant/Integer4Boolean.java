1ccfe2dbf581409ac56f6e86f365c7f427c273df
dnslin <i@dnsl.in>
2024-03-10 19:15:11 +0800

    feat: 增加数字工具类

---
+++ src/main/java/in/dnsl/constant/Integer4Boolean.java
@@ -0,0 +1,37 @@
+package in.dnsl.constant;
+
+import lombok.Data;
+
+@Data
+public class Integer4Boolean{
+
+    /*
+     * true
+     */
+    public static final Integer STATUS_TRUE = 0;
+
+    /*
+     * false
+     */
+    public static final Integer STATUS_FALSE = 1;
+
+
+    public static final Integer INTEGER_ZERO = 0;
+    public static final Integer INTEGER_ONE = 1;
+
+    public static final Integer INTEGER_TWO = 2;
+
+    public static final Integer INTEGER_THREE = 3;
+    public static final Integer INTEGER_FOUR = 4;
+
+    public static final Integer INTEGER_FIVE = 5;
+    public static final Integer INTEGER_SIX = 6;
+
+    public static final Integer INTEGER_SEVEN = 7;
+
+    public static final Integer INTEGER_EIGHT = 8;
+
+    public static final Integer INTEGER_NINE = 9;
+
+    public static final Integer INTEGER_TEN = 10;
+}
