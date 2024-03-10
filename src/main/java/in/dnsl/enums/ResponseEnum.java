fd55286c7f4aec80d5c248ddc59ac23d2dc44c29
dnslin <i@dnsl.in>
2024-03-10 19:24:10 +0800

    fix: 增加注解枚举

--- src/main/java/in/dnsl/enums/ResponseEnum.java
+++ src/main/java/in/dnsl/enums/ResponseEnum.java
@@ -1,28 +1,15 @@
 package in.dnsl.enums;
 
-public enum ResponseEnum {
+import lombok.Getter;
 
+
+@Getter
+public enum ResponseEnum {
     SUCCESS("200","success"),
     SYSTEM_ERROR("500","发生未知异常。。。");
 
-    private String code;
-    private String message;
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
+    private final String code;
+    private final String message;
 
     ResponseEnum(String code, String message) {
         this.code = code;
