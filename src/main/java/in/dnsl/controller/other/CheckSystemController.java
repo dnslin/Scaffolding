a52e1b619118da9ae3f05cdd762b58fc4760ff2f
dnslin <i@dnsl.in>
2024-03-24 16:16:23 +0800

    feat: 增加检测redis是否在线的接口

---
+++ src/main/java/in/dnsl/controller/other/CheckSystemController.java
@@ -0,0 +1,27 @@
+package in.dnsl.controller.other;
+
+import in.dnsl.core.WrapMapper;
+import in.dnsl.core.Wrapper;
+import in.dnsl.utils.RedisUtils;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@Slf4j
+@Controller
+@RestController
+@RequiredArgsConstructor
+public class CheckSystemController {
+
+    private final RedisUtils redisUtils;
+    @GetMapping("/redis-health")
+    public Wrapper<?> checkRedisHealth() {
+        try {
+            redisUtils.set("redis-health-check", "OK");
+        } catch (Exception e) {
+            return WrapMapper.error("Redis health check failed.");
+        }
+        return WrapMapper.ok("Redis health check passed.");
+    }
+}
