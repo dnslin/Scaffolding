672e6f2f5abb1a0f9b3e68efd8378623a602ce8e
dnslin <i@dnsl.in>
2024-04-01 15:47:12 +0800

    feat: 增加图片的数据结构

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -40,3 +40,28 @@ CREATE TABLE site_config
     contactEmail    TEXT,
     announcement    TEXT -- 新增的公告字段
 );
+
+CREATE TABLE IF NOT EXISTS images
+(
+    id             INTEGER PRIMARY KEY AUTOINCREMENT,
+    userId         INTEGER     NOT NULL,  --用户id
+    domain         TEXT        NOT NULL,  -- 图片域名
+    directory      TEXT        NOT NULL, -- 图片目录
+    album          TEXT        NOT NULL, -- 图片相册
+    createDate     DATE        NOT NULL, -- 创建时间
+    modifyDate     DATE        NOT NULL, -- 修改时间
+    fileName       TEXT        NOT NULL, -- 文件名
+    encryptedKey   TEXT UNIQUE NOT NULL, -- 加密key 防止源地址泄露
+    size           INTEGER     NOT NULL, -- 图片大小
+    info           TEXT, -- 图片信息
+    tags           TEXT, -- 图片标签
+    visibility     TEXT        NOT NULL, -- 是否私密图片 private public ，private则需要密码访问
+    category       TEXT,                 -- 分类字段
+    accessPassword TEXT                 -- 访问密码字段
+);
+
+CREATE INDEX idx_userId ON images (userId);
+CREATE INDEX idx_visibility ON images (visibility);
+CREATE INDEX idx_category ON images (category);
+CREATE INDEX idx_createDate ON images (createDate);
+CREATE INDEX idx_modifyDate ON images (modifyDate);
