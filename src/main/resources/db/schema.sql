89826d2c5674e95d43b9db639afbbbe76ffb3eb1
dnslin <i@dnsl.in>
2024-04-01 15:58:05 +0800

    fix: 优化一下表结构信息

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -44,8 +44,8 @@ CREATE TABLE site_config
 CREATE TABLE IF NOT EXISTS images
 (
     id             INTEGER PRIMARY KEY AUTOINCREMENT,
-    userId         INTEGER     NOT NULL,  --用户id
-    domain         TEXT        NOT NULL,  -- 图片域名
+    userId         INTEGER     NOT NULL, --用户id
+    domain         TEXT        NOT NULL, -- 图片域名
     directory      TEXT        NOT NULL, -- 图片目录
     album          TEXT        NOT NULL, -- 图片相册
     createDate     DATE        NOT NULL, -- 创建时间
@@ -53,11 +53,11 @@ CREATE TABLE IF NOT EXISTS images
     fileName       TEXT        NOT NULL, -- 文件名
     encryptedKey   TEXT UNIQUE NOT NULL, -- 加密key 防止源地址泄露
     size           INTEGER     NOT NULL, -- 图片大小
-    info           TEXT, -- 图片信息
-    tags           TEXT, -- 图片标签
-    visibility     TEXT        NOT NULL, -- 是否私密图片 private public ，private则需要密码访问
+    info           TEXT,                 -- 图片信息
+    tags           TEXT,                 -- 图片标签
+    visibility     INTEGER        NOT NULL, -- 是否私密图片 0:private 1:public ，private则需要密码访问
     category       TEXT,                 -- 分类字段
-    accessPassword TEXT                 -- 访问密码字段
+    accessPassword TEXT                  -- 访问密码字段
 );
 
 CREATE INDEX idx_userId ON images (userId);
