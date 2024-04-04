f3427b5c559eb8d5c7adf60f0aa93fda3616b56b
dnslin <i@dnsl.in>
2024-04-04 22:42:42 +0800

    feat: 增加一些初始化数据

---
+++ src/main/java/in/dnsl/config/RedisInitializer.java
@@ -0,0 +1,27 @@
+package in.dnsl.config;
+
+import in.dnsl.constant.RedisKeyConstant;
+import in.dnsl.repository.UserRepository;
+import in.dnsl.utils.RedisUtils;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class RedisInitializer implements CommandLineRunner {
+
+    private final RedisUtils redisUtils;
+
+    private final UserRepository repository;
+    @Override
+    public void run(String... args) {
+        repository.findAllByEnabled(true).forEach(user -> {
+            redisUtils.set(String.format(RedisKeyConstant.USER_INFO_KEY, user.getId()), user.getUsername());
+            log.info("用户信息缓存成功: {}", user.getUsername());
+        });
+    }
+}
--- src/main/java/in/dnsl/constant/RedisKeyConstant.java
+++ src/main/java/in/dnsl/constant/RedisKeyConstant.java
@@ -7,4 +7,8 @@ public class RedisKeyConstant {
 
     // 用户token  用户id:token
     public static final String TOKEN_KEY = "PicManger:token:user:%s:token:%s";
+
+
+    // 用户信息  用户id
+    public static final String USER_INFO_KEY = "PicManger:user:info:%s";
 }
