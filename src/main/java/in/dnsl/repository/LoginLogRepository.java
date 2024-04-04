252f20a04487dbf82d504f882fa928d4105b5039
dnslin <i@dnsl.in>
2024-04-04 22:42:18 +0800

    fix:修改登录日志表为通用日志表

--- src/main/java/in/dnsl/repository/LoginLogRepository.java
+++
@@ -1,8 +0,0 @@
-package in.dnsl.repository;
-
-import in.dnsl.model.entity.LoginLog;
-import org.springframework.data.repository.Repository;
-
-public interface LoginLogRepository extends Repository<LoginLog, Long> {
-
-}
