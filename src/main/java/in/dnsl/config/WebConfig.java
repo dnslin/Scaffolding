6e9835cad2e891f4e788ec442135b56cc760c69c
dnslin <i@dnsl.in>
2024-04-05 00:29:18 +0800

    docs: 增加加载注释

--- src/main/java/in/dnsl/config/WebConfig.java
+++ src/main/java/in/dnsl/config/WebConfig.java
@@ -1,10 +1,12 @@
 package in.dnsl.config;
 
 import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Slf4j
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
@@ -13,6 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+        log.info("注册拦截器已经生效......");
         registry.addInterceptor(blacklistInterceptor); // 注册拦截器
     }
 }
