51f334597b7f029473a53852327ddfe901dba6a0
dnslin <i@dnsl.in>
2024-04-05 14:15:44 +0800

    fix: 预重构操作日志注解

--- src/main/java/in/dnsl/Application.java
+++ src/main/java/in/dnsl/Application.java
@@ -15,6 +15,5 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-        System.out.println("Application started successfully!");
     }
 }
